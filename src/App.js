942f2e3a2af6a07dbedd8b8a016b1ffa5c38c014
Pekstaar <pekstaar@gmail.com>
2021-03-28 00:24:07 +0300

    posts

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
         {/* feed */}
         <Feed />
         {/* widgets */}
+        <Widgets />
       </div>
     </div>
   );
--- src/Feed.js
+++ src/Feed.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Feed.css";
+import MessageSender from "./MessageSender";
+import Post from "./Post";
 import StoryReel from "./StoryReel";
 
 const File = () => {
@@ -8,6 +10,22 @@ const File = () => {
       {/* Story Reel */}
       <StoryReel />
       {/* MessageSender */}
+      <MessageSender />
+      <Post
+        profielPic="https://expertphotography.com/wp-content/uploads/2018/10/cool-profile-pictures-retouching-1.jpg"
+        message="Nice Coding by Pekstar Coders"
+        timestamp="12:55:12"
+        username="Mary Sherryl"
+        image="https://images.idgesg.net/images/article/2017/06/reactjs_code_coding_thinkstock-100725807-large.jpg"
+      />
+      <Post
+        profielPic="https://img.etimg.com/thumb/width-1200,height-900,imgsize-594328,resizemode-1,msid-69381980/tech/internet/in-webs-dark-corner-your-profile-is-on-sale-for-just-a-few-bucks.jpg"
+        message="The application actually Runs?"
+        timestamp="22:55:12"
+        username="Jack Anderson"
+      />
+      {/* <Post />
+      <Post /> */}
     </div>
   );
 };
